604b02b6d77fa2aa9f6ac18642f35695f1a2b894
Napatjira P. <chuuxpormor@gmail.com>
2025-12-17 22:16:41 +0700

    Add files via upload

---
+++ js/cct.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+  //hides dropdown content
+  $(".size_chart").hide();
+  
+  //unhides first option content
+  $("#option1").show();
+  
+  //listen to dropdown for change
+  $("#size_select").change(function(){
+    //rehide content on change
+    $('.size_chart').hide();
+    //unhides current item
+    $('#'+$(this).val()).show();
+  });
+  
+});
